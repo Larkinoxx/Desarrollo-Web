07d36cac02277cad8efd6db0db259659cc399c38
Larkinoxx <juanjocasas04@gmail.com>
2023-08-01 11:36:23 -0500

    Ejemplo JS
    Tarea basicos de JS

---
+++ class.js
@@ -0,0 +1,39 @@
+let player = 'Juan Casas';
+player = 2023;
+
+var playerDos = 'Otro Gato';
+
+//Imprime el valor de playerDos
+console.log(playerDos.length);
+
+//creacion de variales
+//el comando let permite crear variables de cualquier tipo, sea int, string, etc
+let a;
+a = 3.1415
+
+//Boolean constante, es decir, const no permite cambiar el valor interno de su variable
+const found = false;
+
+//no recomendable usar debido a su vejez, mejor usar let
+var age = 3;
+
+/* Comentario
+multi
+linea */
+
+let n = 0
+
+n += 23448
+
+//console.log() imprime en consola
+console.log(n);
+
+let result;
+
+result = `el player dos de nombre ${playerDos} con edad ${age} y numero de prueba ${n}`
+
+console.log(result);
+
+
+//CONDICIONALES
+// Se usa || para condicional OR
