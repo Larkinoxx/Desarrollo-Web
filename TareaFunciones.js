baf5d88471c25a33643e554be0e5c13737472780
Larkinoxx <juanjocasas04@gmail.com>
2023-08-08 16:16:34 -0500

    TareaFunciones.js
    Practica de uso de funciones y arreglos en JS

--- TareaFunciones.js
+++ TareaFunciones.js
@@ -1,6 +1,6 @@
 let datosPersonales = ['Luis', '21/09/2002', 31];
 
-console.log(datosPersonales);
+console.log("Datos base: ", datosPersonales);
 
 const correccionEdad = edad =>{
   datosPersonales.pop();
@@ -9,16 +9,18 @@ const correccionEdad = edad =>{
 
 correccionEdad(21);
 
-console.log(datosPersonales);
+console.log("Datos actualizados: ", datosPersonales);
 
 const añoNacimiento = (edad, año) => año-edad;
 
+console.log("Año nacimiento: ", añoNacimiento(21, 2023));
+
 const comprobacionDatos = (edad, año, funcion1, funcion2) =>{
-  let correcion = funcion1(edad);
+  let correccion = funcion1(edad);
   let nacimiento = funcion2(edad,año);
-  console.log(datosPersonales);
-  console.log(nacimiento);
+  console.log("Datos actualizados durante una funcion callback: ", datosPersonales);
+  console.log("Año de nacimiento durante una funcion callback: ", nacimiento);
   return nacimiento;
 }
 
-console.log(comprobacionDatos(41, 2023, correccionEdad,añoNacimiento));
+console.log("Llamado a 2 funciones a traves de un callback: ", comprobacionDatos(41, 2023, correccionEdad, añoNacimiento), datosPersonales);
