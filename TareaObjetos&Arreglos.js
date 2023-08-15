d3b65ab9d0c8bc23eab4372c92f387e9c385e4c6
Larkinoxx <juanjocasas04@gmail.com>
2023-08-15 11:29:32 -0500

    Tarea Objetos y Arreglos
    Tarea de objetos y arreglos con condiciones especificas comentadas dentro del código.

---
+++ TareaObjetos&Arreglos.js
@@ -0,0 +1,12 @@
+/* Construir Array con minimo 2 objetos.
+*  Cada objeto debe tener adentro otro objeto
+*  Usar minimo 3 metodos de arreglos sobre el array */
+
+/* Array de casa con color, ciudad, pisos, personas
+que viven dentro, y para personas ocupacion, edad, sexo*/
+
+/* Array de dispositivos moviles con marca, precio, tiempo de
+uso, y modelo con color, ram, almacenamiento, flash frontal
+tipo true o false */
+
+// Arreglos .forEach, .filter y .every
